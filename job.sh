f9a387801aa03e1505f2f1ce54494af125b9c4b7
DreamSnakeMeng <117662398+DreamSnakeMeng@users.noreply.github.com>
2024-07-19 18:19:55 +0800

    Delete job.sh

--- job.sh
+++
@@ -1,5 +0,0 @@
-echo "#!/bin/sh">submit.sh
-echo "yhrun -N 1 -n 16 -p work ./DNS >out 2>&1">>submit.sh
-chmod 700 submit.sh 
-yhbatch -N 1 -n 16 -p work -J SPE-NS_test.mzy  ./submit.sh
-rm submit.sh
